7ea2860cbe7bad99063ac1881499fc8bf90d0eb6
유현민 <yhm8622@simsimi.com>
2022-10-01 21:51:25 +0900

    feat: add db connection to middleware
    - add session check by communicating with session db
    - renew session expire time if session is valid
    - fix no session checking url for login since it became get method and before method will not work

--- app/middleware/middleware.py
+++ app/middleware/middleware.py
@@ -1,7 +1,9 @@
+from datetime import datetime, timedelta
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from fastapi import Request
 from starlette.responses import Response, JSONResponse
 from fastapi.responses import RedirectResponse
+import app.database.sql_executer as db
 
 class Middleware(BaseHTTPMiddleware):
     """
@@ -18,36 +20,61 @@ class Middleware(BaseHTTPMiddleware):
             sessionID = request.headers['sessionID']
             # print(f"Header : uid: {uid}, sessionID: {sessionID}")
         except Exception as e:
-            print(f"Header : error. {e}")
+            print(f"Header error. {e}")
             # TODO : delete this part and send error log after testing
-            uid = ""
-            sessionID = ""
+            uid = 1
+            sessionID = 9
 
             # return JSONResponse(status_code=400, content= "Header Error")
 
-        if(route == "auth/login" or route == "docs" or route == "openapi.json"):
+        if(route[0:10] == "auth/login" or route == "docs" or route == "openapi.json"):
             # do nothing
             print("session check pass route")
 
         else :
-            if not await session_check(uid=uid, sessionID=sessionID):
+            if not await session_check(uid, sessionID):
                 # wrong session id
-                res = RedirectResponse("/auth/login")
+                res = JSONResponse(status_code= 440, content= f"Session Expired",)
                 return res
-        
         
         # 실행
         try:
-            print("berfore call next")
             res: Response = await call_next(request)
-            print("affter call next")
         except Exception as e:
             res = JSONResponse(status_code= 500, content= f"Internal Server Error {e}",)
 
         return res
 
 
-async def session_check(uid: str, sessionID: str) -> bool:
-    # compare with session data
-    # TODO : check session validation
-    return True
+async def session_check(uid: int, sessionID: int) -> bool:
+    # check session validation
+    sql = """
+            select *
+            from wdygo.session
+            where session_id = %s and uid = %s
+        """
+    values = (sessionID, uid)
+
+    result = await db.sql_read(sql, values)
+
+    if len(result) > 0:
+        res = result[0]
+        if res["expire_date"]>datetime.now():
+            await refresh_session(sessionID)
+            return True
+
+
+    # if result["expire_date"] > datetime.now():
+
+    return False
+
+async def refresh_session(sessionID: int):
+    sql = """
+            UPDATE wdygo.session
+            SET expire_date = %s
+            WHERE session_id = %s
+        """
+    ten_minute_from_now = datetime.now() + timedelta(minutes = 10)
+    values = (ten_minute_from_now, sessionID)
+
+    result = await db.sql_write(sql, values)
