31ad6ec6e93d54b7fd2b5866b30010403c9ba602
유현민 <yhm8622@simsimi.com>
2022-10-03 20:50:04 +0900

    feat: add redis for favorite related functions
    - get, set, del command for favorite connected to redis
    - in get, first search for result in redis and if there is no hit go to DB

--- app/routes/favorite.py
+++ app/routes/favorite.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 from urllib import response
 from app.models.favorite_model import GetFavoritesResp, PostFavoriteReq
@@ -9,24 +10,37 @@ import app.main as main
 router = APIRouter(prefix='/favorite')
 
 
-@router.get('/favorite', response_model=GetFavoritesResp)
+@router.get('/favorite')
 async def get_favorites(uid: int):
-    sql = """
-            SELECT id AS favorite_id, station_id
-            FROM wdygo.favorites
-            WHERE uid = %s
-        """
-    values = (uid)
 
-    result = await db.sql_read(sql, values)
+    redis_hit = await redis_get_favorite_by_uid(uid)
 
-    if isinstance(result, Exception):
-        raise HTTPException(status_code=500, detail=str(result))
+    if len(redis_hit)>0:
+        return ORJSONResponse(status_code=200, content= {"favorite_list":redis_hit}) 
 
-    if len(result) > 0 :
-        return ORJSONResponse(status_code=200, content= {"favorite_list":result}) 
     else :
-        return HTTPException(status_code=404, detail="User Has No Favorites")
+        sql = """
+                SELECT id AS favorite_id, station_id
+                FROM wdygo.favorites
+                WHERE uid = %s
+            """
+        values = (uid)
+
+        result = await db.sql_read(sql, values)
+
+        if isinstance(result, Exception):
+            raise HTTPException(status_code=500, detail=str(result))
+
+        if len(result) > 0 :
+            for i in range(0,len(result)):
+                await redis_set_favorite(
+                    result[i]["favorite_id"],
+                    result[i]["station_id"],
+                    uid
+                )
+            return ORJSONResponse(status_code=200, content= {"favorite_list":result})
+        else :
+            return ORJSONResponse(status_code=404, content="User Has No Favorites")
 
 
 @router.post('/favorite')
@@ -43,6 +57,8 @@ async def add_favorite(req: PostFavoriteReq):
 
     if isinstance(result, Exception):
         raise HTTPException(status_code=500, detail=str(result))
+
+    await redis_set_favorite(result, req.station_id, req.uid)
 
     return ORJSONResponse(status_code=200, content= "Success") 
 
@@ -66,19 +82,39 @@ async def delete_favorite(uid: int, favorite_id: int):
     if result == None:
         return ORJSONResponse(status_code=404, content= "No Item to be deleted")
 
+    await redis_delete_favorite(favorite_id)
+
     return ORJSONResponse(status_code=200, content= "Success") 
 
 
-@router.get("/redis_test")
-async def redis_test():
-    print("redis test start")
-    redis = main.app.state.redis # set
-    await redis.set(
-        "apple",
-        "banana",
-    )
-    print("affter pool set")
-    print("redis affter set")
 
-    return await redis.get("apple")
+async def redis_set_favorite(id:int, station_id:int, uid:int):
+    redis = main.app.state.redis
+    res = await redis.execute_command('JSON.SET', f'id:{id}', "$", f'{{"station_id":{station_id}, "uid":{uid}}}')
+
+    return res
+
+async def redis_delete_favorite(favorite_id: int):
+    redis = main.app.state.redis
+    res = await redis.execute_command('JSON.DEL', f'id:{favorite_id}')
+
+    return res
+
+
+async def redis_get_favorite_by_uid(uid:int):
+    redis = main.app.state.redis
+    res = await redis.execute_command('FT.SEARCH', 'fvrIndex', f'@uid:[{uid} {uid}]')
+
+    num_result = res[0]
+
+    favorite_list = []
+    for i in range(0,num_result):
+        favorite_list.append({
+            "favorite_id": int(res[2*i+1].split(":")[1]),
+            "station_id": json.loads(res[2*i+2][1])["station_id"]
+        })
+
+    return favorite_list
+    
+
 	
